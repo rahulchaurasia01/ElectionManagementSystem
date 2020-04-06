63c5ff4fafadaf6f368e1440d4f0f3d6aeae6aa2
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-06 18:03:43 +0530

    Added Sagger and DI for Party and Election Interface.

--- EmsBackend/EmsBackend/Startup.cs
+++ EmsBackend/EmsBackend/Startup.cs
@@ -7,8 +7,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using EmsBusinessLayer.Interface;
+using EmsBusinessLayer.Services;
+using EmsRepositoryLayer.Interface;
+using EmsRepositoryLayer.Services;
 
 namespace EmsBackend
 {
@@ -25,11 +30,30 @@ namespace EmsBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ems Api", Version = "v1" });
+            });
+
+            services.AddTransient<IPartyBusiness, PartyBusiness>();
+            services.AddTransient<IPartyRepository, PartyRepository>();
+
+            services.AddTransient<IElectionBusiness, ElectionBusiness>();
+            services.AddTransient<IElectionRepository, ElectionRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+           {
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ems Api v1");
+               c.RoutePrefix = string.Empty;
+           });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
