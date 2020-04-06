70398e2a2e5416a512e10212a0ab3763a3688555
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-06 18:19:16 +0530

    Model for Request and Response for party and election.

---
+++ EmsBackend/EmsCommonLayer/Model/RequestModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmsCommonLayer.Model
+{
+
+    /// <summary>
+    /// It's a model for PartyWise Election Result
+    /// </summary>
+    public class PartyWiseRequestModel
+    {
+
+        public string State { set; get; }
+
+    }
+
+    /// <summary>
+    /// It's a model for Creating a new Party
+    /// </summary>
+    public class CreatePartyRequestModel
+    {
+
+        public string Name { set; get; }
+
+    }
+
+}
---
+++ EmsBackend/EmsCommonLayer/Model/ResponseModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmsCommonLayer.Model
+{
+
+    /// <summary>
+    /// It's the Response Model For viewing party wise election result.
+    /// </summary>
+    public class PartyResponseModel
+    {
+
+        public string Name { set; get; }
+
+        public int Won { set; get; }
+
+        public int Leading { set; get; }
+
+    }
+
+    /// <summary>
+    /// It's the Response Model When a new party is created.
+    /// </summary>
+    public class CreatePartyResponseModel
+    {
+
+        public int PartyId { set; get; }
+
+        public string Name { set; get; }
+
+        public DateTime CreatedAt { set; get; }
+
+        public DateTime ModifiedAt { set; get; }
+
+    }
+
+}
