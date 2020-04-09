6fcb89c9f5f9e19ea914d6f5ab2c1a5e9285df21
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-09 17:01:33 +0530

    Stored Procedure for City done.

--- EmsDatabase/3. Stored Procedure/spCity.sql
+++ EmsDatabase/3. Stored Procedure/spCity.sql
@@ -18,7 +18,8 @@ Begin
 				End
 			else
 				Begin
-					select @CityPresentCount = Count(Name) from City where Name = @Name and StateId = @StateId
+					select @CityPresentCount = Count(Name) from City
+					where Name = @Name and StateId = @StateId
 
 					if (@CityPresentCount > 0)
 						Begin
@@ -27,10 +28,69 @@ Begin
 						End
 					else
 						Begin
-							insert into City(Name, StateId, CreatedAt, ModifiedAt) values(@Name, @StateId, 
-								GETDATE(), GETDATE())
+							insert into City(Name, StateId, CreatedAt, ModifiedAt)
+							values(@Name, @StateId, GETDATE(), GETDATE())
+
 							select * from City where CityId = IDENT_CURRENT('City')
 						End
 				End
+		End
+	else if (@ActionType = 'GetAllCity')
+		Begin
+			select * from City 
+		End
+	else if (@ActionType = 'GetCityById')
+		Begin
+			select * from City where CityId = @CityId 
+		End
+	else if (@ActionType = 'GetAllCityWithStateName')
+		Begin
+			select CityId, city.Name 'City Name' , state.Name as 'State Name', city.CreatedAt, city.ModifiedAt
+			from City as city
+			inner join State as state
+			on city.StateId = state.StateId
+		End
+	else if (@ActionType = 'GetCityWithStateNameByCityId')
+		Begin
+			select CityId, city.Name 'City Name' , state.Name as 'State Name', city.CreatedAt, city.ModifiedAt
+			from City as city
+			inner join State as state
+			on city.StateId = state.StateId
+			where CityId = @CityId
+		End
+	 else if (@ActionType = 'Update')
+		Begin
+			Declare @CityNamePresentCount int
+
+			select @CityNamePresentCount = Count(*) from State Where StateId = @StateId
+
+			if (@CityNamePresentCount = 0)
+				Begin
+					set @CityNamePresentCount = -1
+					return @CityNamePresentCount
+				End
+			else
+				Begin
+					select @CityNamePresentCount = Count(Name) from City
+					where Name = @Name and StateId = @StateId
+
+					if (@CityNamePresentCount > 0)
+						Begin
+							set @CityNamePresentCount = -2
+							return @CityNamePresentCount
+						End
+					else
+						Begin
+							update City
+							set Name = @Name, StateId = @StateId, ModifiedAt = GETDATE()
+							where CityId = @CityId
+
+							select * from City where CityId = @CityId
+						End
+				End
+		End
+	else if (@ActionType = 'Delete')
+		Begin
+			delete from City where CityId = @CityId
 		End
 End
