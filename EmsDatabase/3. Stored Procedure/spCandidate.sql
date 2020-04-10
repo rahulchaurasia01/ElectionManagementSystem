3b01d9d98802b3ad3e09fd4e8a470073965c1548
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-10 12:33:30 +0530

    Create stored procedure for CRUD Operation on candidate

--- EmsDatabase/3. Stored Procedure/spCandidate.sql
+++ EmsDatabase/3. Stored Procedure/spCandidate.sql
@@ -1,4 +1,5 @@
 create procedure spCandidate
+@CandidateId int,
 @Name varchar(50),
 @ConstituencyId int,
 @PartyId int,
@@ -9,14 +10,30 @@ Begin
 		Begin
 			Declare @CandidatePresentCount int
 
-			select @CandidatePresentCount = Count(Name) from Constituency
-				where ConstituencyId = @ConstituencyId
+			select @CandidatePresentCount = Count(Name)
+			from Constituency
+			where ConstituencyId = @ConstituencyId
 
-			print @CandidatePresentCount
+			if (@CandidatePresentCount = 0)
+				Begin
+					set @CandidatePresentCount = -1
+					return @CandidatePresentCount
+				End
 
-			Select @CandidatePresentCount = Count(Name) from Candidate
-				Where ConstituencyId = @ConstituencyId and PartyId = @PartyId
+			Select @CandidatePresentCount = Count(Name)
+			from Party
+			where PartyId = @PartyId
+
+			if (@CandidatePresentCount = 0)
+				Begin
+					set @CandidatePresentCount = -2
+					return @CandidatePresentCount
+				End
 			
+			Select @CandidatePresentCount = Count(Name)
+			from Candidate
+			Where ConstituencyId = @ConstituencyId and PartyId = @PartyId
+
 			If (@CandidatePresentCount = 0)
 				Begin
 					insert into Candidate(Name, ConstituencyId, PartyId, CreatedAt, ModifiedAt)
@@ -27,8 +44,42 @@ Begin
 				End
 			Else
 				Begin
+					set @CandidatePresentCount = -3
 					return @CandidatePresentCount
 				End
 		End
+	else if (@ActionType = 'GetAll')
+		Begin
+			select * from Candidate
+		End
+	else if (@ActionType = 'GetCandidateById')
+		Begin
+			select * from Candidate where CandidateId = @CandidateId
+		End
+	else if (@ActionType = 'Update')
+		Begin
+			Declare @CandidateNamePresentCount int
 
+			select @CandidateNamePresentCount = Count(Name)
+			from Candidate
+			where CandidateId = @CandidateId
+
+			if (@CandidateNamePresentCount = 0)
+				Begin
+					set @CandidateNamePresentCount = -1
+					return @CandidateNamePresentCount
+				End
+			else
+				Begin
+					Update Candidate
+					set Name = @Name, ModifiedAt = GETDATE()
+					where CandidateId = @CandidateId
+
+					Select * from Candidate where CandidateId = @CandidateId
+				End
+		End
+	else if (@ActionType = 'Delete')
+		Begin
+			Delete from Candidate where CandidateId = @CandidateId
+		End
 End
