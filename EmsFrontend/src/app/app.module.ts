90565c63ecbabf24dd65b6d10acd4092d5b8a79b
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-16 18:31:09 +0530

    Added Imports needed for the running of the application

--- EmsFrontend/src/app/app.module.ts
+++ EmsFrontend/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ElectionComponent } from './components/election/election.component';
+import { AppRoutingModule } from './routing/app.routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElectionComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
